a3bfde4b0ca0cc230369235cbe359738b81bed95
Utfyller Publisher <syver.enstad+fyllutpublish@gmail.com>
2021-06-04 15:07:20 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
 
 function App() {
+  const [files, setFiles] = useState([]);
+  const setFilesHelper = (fileList) => {
+    const fileArray = [];
+    for (let i = 0; i < fileList.length; i++) {
+      fileArray.push(fileList[i]);
+    }
+    setFiles(fileArray);
+  }
+  console.log('files state', files);
+
+  function handleFiles(event) {
+    setFilesHelper(event.target.files);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form>
+        <input type="file" id="fileInput" multiple onChange={handleFiles}/>
+      </form>
+      <ul>
+        {files.map(file => {
+          const src = URL.createObjectURL(file);
+          let previewComponent;
+          if (file.type.startsWith('image/')) {
+            previewComponent = <img alt="flesk" src={src} height={60} onLoad={() => URL.revokeObjectURL(src)} />
+          } else if (file.type.startsWith('application/pdf')) {
+            previewComponent = <iframe title={file.name} src={src} onLoad={() => URL.revokeObjectURL(src)} />
+          } else {
+            previewComponent = <div>{file.type} er ikke støttet</div>
+          }
+          return <li key={src}>
+            <span>
+              {previewComponent}
+              {`${file.name}: ${file.size} bytes`}
+            </span>
+          </li>;
+        })}
+      </ul>
     </div>
   );
 }
